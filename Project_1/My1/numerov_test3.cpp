e5bd0fa6337fcd5fc473fcce756851761fe7ffb9
Filippo Mione <filippo.mione@gmail.com>
2020-03-24 13:25:19 +0100

    added the numerical comparison of bessel functions generated by recursion algorithm in comparison to the analythical ones

---
+++ Project_1/My1/numerov_test3.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+double h = 1e-4;
+double X_MIN = 0.;
+double X_MAX = 7.;
+const int MESH_SIZE = ceil( (X_MAX - X_MIN)/h);
+
+double bessel_j(double x, int l)
+{
+    double j_1,j0,j1;
+
+    j_1=cos(x)/x;
+    j0=sin(x)/x;
+
+    if(l>0)
+    {
+        for(int i=0;i<l;i++)
+        {
+            j1=(2*i+1)/x*j0-j_1;
+            j_1=j0;
+            j0=j1;
+        }
+    }
+    return j0;
+}
+
+double bessel_n(double x, int l)
+{
+    double j_1,j0,j1;
+
+    j_1=sin(x)/x;
+    j0=-cos(x)/x;
+
+    if(l>0)
+    {
+        for(int i=0;i<l;i++)
+        {
+            j1=(2*i+1)/x*j0-j_1;
+            j_1=j0;
+            j0=j1;
+        }
+    }
+    return j0;
+}
+
+
+
+int main()
+{
+    double analyt, calcul;
+    double *x = new double[MESH_SIZE];
+    double *y = new double[MESH_SIZE];
+    FILE *out;
+    out = fopen("test_b.txt","w+");
+
+    cout<<"step size h= "<<h<<endl;
+
+    int i;
+    for(i=0;i<MESH_SIZE;i++)
+    {
+        x[i]= X_MIN +i*h ;
+        fprintf(out, "%10.12lf ",x[i]);
+        calcul = bessel_j(x[i], 1);
+        analyt = sin(x[i])/x[i]/x[i] - cos(x[i])/x[i];
+        fprintf(out, "%10.12lf %10.12lf %10.12lf ", calcul, analyt, calcul-analyt);
+        calcul = bessel_n(x[i], 1);
+        analyt = - cos(x[i])/x[i]/x[i]-sin(x[i])/x[i];
+        fprintf(out, "%10.12lf %10.12lf %10.12lf ", calcul, analyt, calcul-analyt);
+        calcul = bessel_j(x[i], 2);
+        analyt = (3./x[i]/x[i]-1)*sin(x[i])/x[i] - 3*cos(x[i])/x[i]/x[i];
+        fprintf(out, "%10.12lf %10.12lf %10.12lf ", calcul, analyt, calcul-analyt);
+        calcul = bessel_n(x[i], 2);
+        analyt = (-3./x[i]/x[i]+1)* cos(x[i])/x[i]-3*sin(x[i])/x[i]/x[i];
+        fprintf(out, "%10.12lf %10.12lf %10.12lf ", calcul, analyt, calcul-analyt);
+        calcul = bessel_j(x[i], 3);
+        analyt = (15./pow(x[i],3)-6/x[i])*sin(x[i])/x[i] - (15/x[i]/x[i]-1)*cos(x[i])/x[i];
+        fprintf(out, "%10.12lf %10.12lf %10.12lf ", calcul, analyt, calcul-analyt);
+        calcul = bessel_n(x[i], 3);
+        analyt = (-15/pow(x[i],3)+6/x[i])*cos(x[i])/x[i] -(15/x[i]/x[i]-1)* sin(x[i])/x[i];
+        fprintf(out, "%10.12lf %10.12lf %10.12lf\n", calcul, analyt, calcul-analyt);
+    }
+
+    fclose(out);
+}
